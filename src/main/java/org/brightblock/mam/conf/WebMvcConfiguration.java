9cea75428b7ad68854cf961c694f47019d973423
Mike Cohen <mjoecohen@gmail.com>
2018-09-19 13:20:19 +0100

    search index server port changed to 8191 to avoid port collisions

--- src/main/java/org/brightblock/mam/conf/WebMvcConfiguration.java
+++ src/main/java/org/brightblock/mam/conf/WebMvcConfiguration.java
@@ -17,6 +17,6 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
+		registry.addMapping("/**").allowedOrigins("*");
 	}
 }
