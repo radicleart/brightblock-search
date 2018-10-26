559c19caf51d893e4cb1a03cb5e3eb2e01c6e437
Mike Cohen <mjoecohen@gmail.com>
2018-10-26 10:44:56 +0100

    Cors: transit8 cors p[olicy update

--- src/main/java/org/brightblock/mam/conf/SecurityConfiguration.java
+++ src/main/java/org/brightblock/mam/conf/SecurityConfiguration.java
@@ -1,12 +1,9 @@
 package org.brightblock.mam.conf;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -25,10 +22,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		.httpBasic();
     	}
     
-    @Bean
-    UrlBasedCorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-        return source;
-    }
+//    @Bean
+//    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+//        return source;
+//    }
 }
